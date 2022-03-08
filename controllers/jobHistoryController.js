14a36fe632e7e977155febbe3985450c8eff9e07
mostafaElsayed <mostafa.elsayed3537@gmail.com>
2022-03-08 23:48:21 +0200

    contractor history

--- controllers/jobHistoryController.js
+++ controllers/jobHistoryController.js
@@ -5,19 +5,16 @@ const APIFeatures = require('../utils/apiFeatures');
 
 exports.getALlWorlHistoryForContractor = catchAsync(async (req, res, next) => {
   const features = new APIFeatures(
-    JobHistory.find({ jobStaus: 'done' }),
+    JobHistory.find({ jobStaus: 'done', contractor: req.params.contId }),
     req.query
   ).paginate();
-
   const docs = await features.query;
-
   if (!docs)
     return res.status(204).json({
       status: 'Success',
       result: docs.length,
       data: null,
     });
-
   res.status(200).json({
     status: 'Success',
     data: docs,
